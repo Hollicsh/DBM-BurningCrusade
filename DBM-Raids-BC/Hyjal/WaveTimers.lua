575af20c149608486650e90c5d5e316a46d6eef9
MysticalOS <mysticalosx@gmail.com>
2024-05-20 06:30:15 -0400

    Note

--- DBM-Raids-BC/Hyjal/WaveTimers.lua
+++ DBM-Raids-BC/Hyjal/WaveTimers.lua
@@ -2,6 +2,7 @@ local mod	= DBM:NewMod("HyjalWaveTimers", "DBM-Raids-BC", 3)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision("@file-date-integer@")
+--mod:SetZone(1234)--FIXME RIGHT ID
 
 mod:RegisterEvents(
 	"GOSSIP_SHOW",
